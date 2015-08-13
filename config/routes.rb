bf8b42ff15de69f2b51006ba332ebdd901d5a0f4
ndjordjevic <ndjordjevic@gmail.com>
2015-08-13 21:28:04 +0200

    Add routes namespace, defaults and constraints

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  namespace :api, defaults: { formatb: :json }, constraints: { subdomain: 'api' }, path: '/' do
+
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
